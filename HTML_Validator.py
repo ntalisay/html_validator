01e98883d014db0361838fe68242006cd6ad021e
Nicole Talisay <atad2018@mymail.pomona.edu>
2020-02-16 23:01:56 -0800

    create functions

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -10,6 +10,28 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
+    tag_lst = _extract_tags(html)
+    l = []
+    i = 0
+    balanced = True
+    for i in range(len(tag_lst)):
+        head = tag_lst[i]
+        
+        if head[1] != '/': 
+            l.push(tag_lst)
+        else: 
+            if l == []:
+                balanced = False
+            else:
+                top = l.pop() 
+                if top[1:] != head[2:] :
+                    balanced = False
+
+    if balanced and s == []:
+        return True
+    else:
+        return False
+
 
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
@@ -25,7 +47,20 @@ def _extract_tags(html):
 
     This function returns a list of all the html tags contained in the input string,
     stripping out all text not contained within angle brackets.
-
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+
+    out = [] 
+
+    for i in range(len(html)):
+        tag = ""
+        string_ind = html[i]
+        if string_ind  == '<':
+            while string_ind != '>':
+                tag += string_ind
+                string_ind = html[i+1] 
+            out.append(tag)
+        i += 1
+
+    return out 
